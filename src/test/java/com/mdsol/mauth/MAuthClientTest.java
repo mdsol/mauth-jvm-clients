28076b3ff561f4eec57534fe6255c6eef9f4b2fe
mszewczyszyn-mdsol <mszewczyszyn@mdsol.com>
2015-10-13 12:00:33 +0100

    More tests. Wiremock for stubbing MAuth server.

--- src/test/java/com/mdsol/mauth/MAuthClientTest.java
+++ src/test/java/com/mdsol/mauth/MAuthClientTest.java
@@ -10,15 +10,14 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.http.client.methods.HttpPost;
 import org.bouncycastle.openssl.PEMReader;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.io.IOException;
 import java.io.StringReader;
@@ -26,28 +25,42 @@ import java.security.PublicKey;
 
 import javax.servlet.http.HttpServletResponse;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(MAuthClient.class)
+// @RunWith(PowerMockRunner.class)
+// @PrepareForTest(MAuthClient.class)
 public class MAuthClientTest {
 
   @Rule
   public ExpectedException expectedException = ExpectedException.none();
 
   private static final String CLIENT_APP_ID = "162a4b03-4db9-4631-9d1f-d7195f37128d";
-  private static final String CLIENT_SIGNATURE =
+
+  private static final String CLIENT_REQUEST_SIGNATURE =
       "fFQzIOo4S1MxxmEDB9v7v0IYNytnS3I5aHNeJfEfFe1v1gTE/cH36BfLG"
           + "/zpOHX7kPUDUnmVZK1MuqdenLDmY6q2h8BR/1yA7n/dupQfnVZIeaB99GIO"
           + "FJaHT6P6gXHiMTFxgX3Oo/rj97jfDUxaunxnlqnfhHccPxbhiqfcVgHahw6hiX"
           + "x9sAt/iG/Yu7lzZbWx4SWkDyAGvk1I5rpbxcOWLPYad8qpuCFaHPMQJTTze8G02hd5"
           + "ocZDzQS73MOfUqPLfvv7aLRvBS078cqs2uAip84n8bM4qG60gMfoC9kUluza7i9poyFqq"
           + "IdsCnS5RQuyNcsixneX2X3CNt3yOw==";
+  private static final String CLIENT_REQUEST_METHOD = HttpPost.METHOD_NAME;
+  private static final String CLIENT_REQUEST_PATH = "/resource/path";
+  private static final String CLIENT_REQUEST_BODY = "message here";
+  private static final String CLIENT_X_MWS_TIME_HEADER_VALUE = "1444672122";
 
-  private static final String X_MWS_AUTHENTICATION_HEADER_NAME = "X-Mws-Authentication";
-  private static final String X_MWS_AUTHENTICATION_HEADER_VALUE =
-      "MWS " + CLIENT_APP_ID + ":" + CLIENT_SIGNATURE;
+  private static final String CLIENT_UNICODE_REQUEST_SIGNATURE =
+      "5abfJo+LAj/t+27lS8EYU5g8o5pehn518i14/7YZiF2VY4hV4G/33gz5Uae"
+          + "qpSd/G8UH3vl2HDV1oxF8H9CC51wSssHDs31jruKzbAz/bgtnSeTvSfT2XygmKxQ4pmHQC5dAaLfkSiL2KyUxY1dpqjPxoj82F1HlVrr6UTgMkEqEhNBYiY"
+          + "yWAeJHE05Uc+uScef2iOomOzUcXjh1q8LtJqP2555jj8U9gJlMax0xFB1lumnK4U4VcMZ0R2JPCE2MfcIASeDOrPTMzj12MTvCTLtDBiQsgA2+fqGCEcISeQ4"
+          + "jdkd7/Fr6wD91D0KgSidlDvZWV1DBQPa5xPDzW1H24A==";
+  private static final String CLIENT_UNICODE_REQUEST_METHOD = HttpPost.METHOD_NAME;
+  private static final String CLIENT_UNICODE_REQUEST_PATH = "/resource/path";
+  private static final String CLIENT_UNICODE_REQUEST_BODY =
+      "Message with some Unicode characters inside: ș吉ń艾ęتあù";
+  private static final String CLIENT_UNICODE_X_MWS_TIME_HEADER_VALUE = "1444726041";
 
   private static final String X_MWS_TIME_HEADER_NAME = "X-Mws-Time";
-  private static final String X_MWS_TIME_HEADER_VALUE = "1444672122";
+  private static final String X_MWS_AUTHENTICATION_HEADER_NAME = "X-Mws-Authentication";
+  private static final String CLIENT_X_MWS_AUTHENTICATION_HEADER_VALUE =
+      "MWS " + CLIENT_APP_ID + ":" + CLIENT_REQUEST_SIGNATURE;
 
   private static final String MAUTH_BASE_URL = "http://localhost:9001";
   private static final String MAUTH_URL_PATH = "/mauth/v1";
@@ -57,13 +70,25 @@ public class MAuthClientTest {
   private final String PRIVATE_KEY;
 
   public MAuthClientTest() throws IOException {
-    PRIVATE_KEY = IOUtils.toString(MAuthRequestSigner.class.getResourceAsStream("privatekey.pem"));
-    PUBLIC_KEY = IOUtils.toString(MAuthRequestSigner.class.getResourceAsStream("publickey.pem"));
+    PRIVATE_KEY =
+        IOUtils.toString(MAuthRequestSigner.class.getResourceAsStream("/keys/privatekey.pem"));
+    PUBLIC_KEY =
+        IOUtils.toString(MAuthRequestSigner.class.getResourceAsStream("/keys/publickey.pem"));
   }
 
   private MAuthClient createClientUsingValidTestConfiguration() throws Exception {
     return new MAuthClient(MAUTH_BASE_URL, MAUTH_URL_PATH, SECURITY_TOKENS_PATH, RESOURCE_APP_UUID,
         PUBLIC_KEY, PRIVATE_KEY);
+  }
+
+  @BeforeClass
+  public static void setup() {
+    FakeMAuthServer.start(9001);
+  }
+
+  @AfterClass
+  public static void tearDown() {
+    FakeMAuthServer.stop();
   }
 
   @Test
@@ -102,36 +127,51 @@ public class MAuthClientTest {
   @Test
   public void validatingValidRequestShouldPass() throws Exception {
     // Arrange
-    String signatureInHeader = CLIENT_SIGNATURE;
-    String localTime = X_MWS_TIME_HEADER_VALUE;
-    String verb = "POST";
-    String resourceUrl = "/resource/path";
-    String body = "message here";
-    String appId = CLIENT_APP_ID;
-
-    MAuthClient client = PowerMockito.spy(createClientUsingValidTestConfiguration());
-
-    EpochTime remoteTime = mockEpochTime(Long.valueOf(localTime) + 3);
+    FakeMAuthServer.return200();
+    MAuthClient client = createClientUsingValidTestConfiguration();
+    EpochTime remoteTime = mockEpochTime(Long.valueOf(CLIENT_X_MWS_TIME_HEADER_VALUE) + 3);
     client.setEpochTime(remoteTime);
-
-    PublicKey key = generatePublicKeyFromString(PUBLIC_KEY);
-
-    PowerMockito.doReturn(key).when(client, "getPublicKey", appId);
 
     // Act
     boolean validationResult =
-        client.validateRequest(signatureInHeader, localTime, verb, resourceUrl, body, appId);
+        client.validateRequest(CLIENT_REQUEST_SIGNATURE, CLIENT_X_MWS_TIME_HEADER_VALUE,
+            CLIENT_REQUEST_METHOD, CLIENT_REQUEST_PATH, CLIENT_REQUEST_BODY, CLIENT_APP_ID);
 
     // Assert
     assertThat(validationResult, equalTo(true));
   }
 
-  private PublicKey generatePublicKeyFromString(final String publicKey) throws IOException {
-    PublicKey key = null;
-    try (PEMReader reader = new PEMReader(new StringReader(publicKey))) {
-      key = (PublicKey) reader.readObject();
-    };
-    return key;
+  @Test
+  public void validatingValidRequestWithSpecialCharactersShouldPass() throws Exception {
+    // Arrange
+    FakeMAuthServer.return200();
+    MAuthClient client = createClientUsingValidTestConfiguration();
+    EpochTime remoteTime = mockEpochTime(Long.valueOf(CLIENT_X_MWS_TIME_HEADER_VALUE) + 3);
+    client.setEpochTime(remoteTime);
+
+    // Act
+    boolean validationResult = client.validateRequest(CLIENT_UNICODE_REQUEST_SIGNATURE,
+        CLIENT_UNICODE_X_MWS_TIME_HEADER_VALUE, CLIENT_UNICODE_REQUEST_METHOD,
+        CLIENT_UNICODE_REQUEST_PATH, CLIENT_UNICODE_REQUEST_BODY, CLIENT_APP_ID);
+
+    // Assert
+    assertThat(validationResult, equalTo(true));
+  }
+
+  @Test
+  public void validatingRequestSentAfter5MinutesShouldFail() throws Exception {
+    // Arrange
+    FakeMAuthServer.return200();
+    MAuthClient client = createClientUsingValidTestConfiguration();
+    EpochTime remoteTime = mockEpochTime(Long.valueOf(CLIENT_X_MWS_TIME_HEADER_VALUE) + 600);
+    client.setEpochTime(remoteTime);
+
+    // Act & Assert
+    expectedException.expect(Exception.class);
+    expectedException.expectMessage("validateRequest error: epoc time is older than 5 minutes");
+
+    client.validateRequest(CLIENT_REQUEST_SIGNATURE, CLIENT_X_MWS_TIME_HEADER_VALUE,
+        CLIENT_REQUEST_METHOD, CLIENT_REQUEST_PATH, CLIENT_REQUEST_BODY, CLIENT_APP_ID);
   }
 
   @Test
@@ -140,7 +180,7 @@ public class MAuthClientTest {
     MAuthClient client = createClientUsingValidTestConfiguration();
 
     // Act
-    String appId = client.getAppIdInHeader(X_MWS_AUTHENTICATION_HEADER_VALUE);
+    String appId = client.getAppIdInHeader(CLIENT_X_MWS_AUTHENTICATION_HEADER_VALUE);
 
     // Assert
     assertThat(appId, equalTo(CLIENT_APP_ID));
@@ -164,10 +204,10 @@ public class MAuthClientTest {
     MAuthClient client = createClientUsingValidTestConfiguration();
 
     // Act
-    String signature = client.getSignatureInHeader(X_MWS_AUTHENTICATION_HEADER_VALUE);
+    String signature = client.getSignatureInHeader(CLIENT_X_MWS_AUTHENTICATION_HEADER_VALUE);
 
     // Assert
-    assertThat(signature, equalTo(CLIENT_SIGNATURE));
+    assertThat(signature, equalTo(CLIENT_REQUEST_SIGNATURE));
   }
 
   @Test
