7e9f0d2796d03c95b4191d2017fd6fd3b2d47f7f
Michele Prisco <mprisco@mdsol.com>
2015-10-20 12:11:55 +0100

    MCC-191898: Minor amendments on JavaDocs comments.

--- src/main/java/com/mdsol/mauth/internals/validator/MAuthValidator.java
+++ src/main/java/com/mdsol/mauth/internals/validator/MAuthValidator.java
@@ -1,17 +1,19 @@
 package com.mdsol.mauth.internals.validator;
 
 import com.mdsol.mauth.domain.MAuthRequest;
+import com.mdsol.mauth.exceptions.MAuthValidationException;
 
 public interface MAuthValidator {
 
   /**
-   * Performs the validation of the incoming HTTP request.
+   * Performs the validation of an incoming HTTP request.
    * <p/>
    * The validation process consists of recreating the mAuth hashed signature from the request data
    * and comparing it to the decrypted hash signature from the mAuth header.
    * 
-   * @param mAuthRequest Data from the incoming request necessary to perform the validation.
+   * @param mAuthRequest Data from the incoming HTTP request necessary to perform the validation.
    * @return True or false indicating if the request is valid or not with respect to mAuth.
+   * @throws MAuthValidationException
    */
   boolean validate(MAuthRequest mAuthRequest);
 
