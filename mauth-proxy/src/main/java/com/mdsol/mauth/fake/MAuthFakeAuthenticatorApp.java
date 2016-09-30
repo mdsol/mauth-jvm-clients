693a1ccde05f977951fd3ff9cafd384e25e7ef8b
austek-mdsol <austek@mdsol.com>
2016-09-30 10:53:00 +0100

    Remove unused imports

--- mauth-proxy/src/main/java/com/mdsol/mauth/fake/MAuthFakeAuthenticatorApp.java
+++ mauth-proxy/src/main/java/com/mdsol/mauth/fake/MAuthFakeAuthenticatorApp.java
@@ -6,17 +6,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class MAuthFakeAuthenticatorApp {
