8894490c2e06f5bef8067867a5abfdb14d520076
austek-mdsol <austek@mdsol.com>
2016-10-03 11:42:02 +0100

    extract payload for all calls

--- mauth-proxy/src/main/java/com/mdsol/mauth/proxy/ProxyServer.java
+++ mauth-proxy/src/main/java/com/mdsol/mauth/proxy/ProxyServer.java
@@ -8,6 +8,8 @@ import org.littleshoot.proxy.HttpFilters;
 import org.littleshoot.proxy.HttpFiltersAdapter;
 import org.littleshoot.proxy.HttpFiltersSourceAdapter;
 import org.littleshoot.proxy.impl.DefaultHttpProxyServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -16,6 +18,8 @@ import java.nio.file.Paths;
 import java.util.Map;
 
 public class ProxyServer {
+  private static final Logger logger = LoggerFactory.getLogger(ProxyServer.class);
+
   private final ProxyConfig proxyConfig;
 
   private final HttpClientRequestSigner httpClientRequestSigner;
@@ -38,23 +42,26 @@ public class ProxyServer {
           public int getMaximumRequestBufferSizeInBytes() {
             return proxyConfig.getBufferSizeInByes();
           }
+
           public HttpFilters filterRequest(HttpRequest originalRequest, ChannelHandlerContext ctx) {
             return new HttpFiltersAdapter(originalRequest) {
               @Override
               public HttpResponse clientToProxyRequest(HttpObject httpObject) {
                 if (httpObject instanceof HttpRequest) {
                   HttpRequest request = (HttpRequest) httpObject;
-                  String forwardBaseUrl = request.headers().get("forward-base-url");
-                  if(null == forwardBaseUrl){
-                    forwardBaseUrl = proxyConfig.getForwardBaseUrl();
+                  String forwardBaseUrl = proxyConfig.getForwardBaseUrl();
+                  if (null != request.headers().get("forward-base-url")) {
+                    forwardBaseUrl = request.headers().get("forward-base-url");
+                    logger.debug("Using header forward-base-url: " + forwardBaseUrl);
                   }
                   request.setUri(forwardBaseUrl + request.getUri());
                 }
                 return null;
               }
+
               @Override
               public HttpResponse proxyToServerRequest(HttpObject httpObject) {
-                if(httpObject instanceof FullHttpRequest){
+                if (httpObject instanceof FullHttpRequest) {
                   signRequest((FullHttpRequest) httpObject);
                 }
                 return null;
@@ -67,12 +74,12 @@ public class ProxyServer {
   }
 
   private void signRequest(FullHttpRequest request) {
-    String requestPayload = null;
+    final String requestPayload = request.content().toString(Charset.forName("UTF-8"));
+    final String verb = request.getMethod().name();
+    final String uri = request.getUri();
 
-    if(request.getMethod().equals(HttpMethod.POST) || request.getMethod().equals(HttpMethod.PUT)) {
-      requestPayload = request.content().toString(Charset.forName("UTF-8"));
-    }
-    Map<String, String> mAuthHeaders = httpClientRequestSigner.generateRequestHeaders(request.getMethod().name(), request.getUri(), requestPayload);
+    logger.debug("Generating request headers for Verb: '" + verb + "' URI: '" + uri + "' Payload: " + requestPayload);
+    Map<String, String> mAuthHeaders = httpClientRequestSigner.generateRequestHeaders(verb, uri, requestPayload);
     mAuthHeaders.entrySet().forEach((header) -> request.headers().add(header.getKey(), header.getValue()));
   }
 
