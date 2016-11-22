dd00cb5b16c202b9cfe10299dda71badb17d3113
austek-mdsol <austek@mdsol.com>
2016-11-22 12:25:10 +0000

    Fix HttpClientPublicKeyProvider test

--- mauth-authenticator-apachehttp/src/test/java/com/mdsol/mauth/apache/HttpClientPublicKeyProviderTest.java
+++ mauth-authenticator-apachehttp/src/test/java/com/mdsol/mauth/apache/HttpClientPublicKeyProviderTest.java
@@ -1,6 +1,7 @@
 package com.mdsol.mauth.apache;
 
 import com.github.tomakehurst.wiremock.client.WireMock;
+import com.mdsol.mauth.AuthenticatorConfiguration;
 import com.mdsol.mauth.MAuthConfiguration;
 import com.mdsol.mauth.Signer;
 import com.mdsol.mauth.exception.HttpClientPublicKeyProviderException;
@@ -37,9 +38,6 @@ public class HttpClientPublicKeyProviderTest {
   private static final String MAUTH_BASE_URL = "http://localhost:9001";
   private static final String MAUTH_URL_PATH = "/mauth/v1";
   private static final String SECURITY_TOKENS_PATH = "/security_tokens/%s.json";
-  private static final UUID RESOURCE_APP_UUID = UUID.fromString("92a1869e-c80d-4f06-8775-6c4ebb0758e0");
-
-  private static final String PRIVATE_KEY = FixturesLoader.getPrivateKey();
 
   @BeforeClass
   public static void setup() {
@@ -53,7 +51,7 @@ public class HttpClientPublicKeyProviderTest {
   }
 
   private HttpClientPublicKeyProvider getClientWithMockedSigner() {
-    MAuthConfiguration configuration = getMAuthConfiguration();
+    AuthenticatorConfiguration configuration = getMAuthConfiguration();
 
     Signer mockedSigner = mock(Signer.class);
     Map<String, String> mockedHeaders = new HashMap<>();
@@ -69,8 +67,8 @@ public class HttpClientPublicKeyProviderTest {
     return String.format(MAUTH_URL_PATH + SECURITY_TOKENS_PATH, clientAppId);
   }
 
-  private MAuthConfiguration getMAuthConfiguration() {
-    return new MAuthConfiguration(RESOURCE_APP_UUID, MAUTH_BASE_URL, PRIVATE_KEY, MAUTH_URL_PATH ,SECURITY_TOKENS_PATH, 300L);
+  private AuthenticatorConfiguration getMAuthConfiguration() {
+    return new AuthenticatorConfiguration(MAUTH_BASE_URL, MAUTH_URL_PATH ,SECURITY_TOKENS_PATH, 300L);
   }
 
   @Test
