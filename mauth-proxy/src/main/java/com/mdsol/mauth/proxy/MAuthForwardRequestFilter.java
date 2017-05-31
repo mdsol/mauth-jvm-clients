10cd8329f4ab32b9fbd25dcca64d1f9eba6240af
austek-mdsol <austek@mdsol.com>
2017-05-31 16:45:37 +0100

    Extract MAuthHttpRequestSigner

--- mauth-proxy/src/main/java/com/mdsol/mauth/proxy/MAuthForwardRequestFilter.java
+++ mauth-proxy/src/main/java/com/mdsol/mauth/proxy/MAuthForwardRequestFilter.java
@@ -2,8 +2,8 @@ package com.mdsol.mauth.proxy;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mdsol.util.BuildInfoService;
 import com.mdsol.mauth.Signer;
+import com.mdsol.util.BuildInfoService;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.http.*;
@@ -16,17 +16,16 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 import java.util.Date;
-import java.util.Map;
 
-public class MAuthRequestFilter extends HttpFiltersAdapter {
+public class MAuthForwardRequestFilter extends HttpFiltersAdapter {
 
-  private static final Logger logger = LoggerFactory.getLogger(MAuthRequestFilter.class);
-  private final Signer httpClientRequestSigner;
+  private static final Logger logger = LoggerFactory.getLogger(MAuthForwardRequestFilter.class);
+  private final MAuthHttpRequestSigner requestSigner;
   private final String buildInfo;
 
-  public MAuthRequestFilter(HttpRequest originalRequest, Signer httpClientRequestSigner) {
+  public MAuthForwardRequestFilter(HttpRequest originalRequest, Signer httpClientRequestSigner) {
     super(originalRequest);
-    this.httpClientRequestSigner = httpClientRequestSigner;
+    this.requestSigner = new MAuthHttpRequestSigner(httpClientRequestSigner);
 
     String tmpBuildInfo;
     try {
@@ -60,28 +59,15 @@ public class MAuthRequestFilter extends HttpFiltersAdapter {
   @Override
   public HttpResponse proxyToServerRequest(HttpObject httpObject) {
     if (httpObject instanceof FullHttpRequest) {
-      signRequest((FullHttpRequest) httpObject);
+      final FullHttpRequest request = (FullHttpRequest) httpObject;
+      if (!request.getMethod().name().equalsIgnoreCase("CONNECT")) {
+        requestSigner.signRequest(request);
+      }
     }
     return null;
   }
 
-  private void signRequest(FullHttpRequest request) {
-    final String verb = request.getMethod().name();
 
-    if (!verb.equalsIgnoreCase("CONNECT")) {
-      final String requestPayload = request.content().toString(Charset.forName("UTF-8"));
-      String uri = request.getUri();
-      try {
-        uri = new URI(uri).getPath();
-      } catch (URISyntaxException e) {
-        logger.error("Couldn't get request uri", e);
-      }
-
-      logger.debug("Generating request headers for Verb: '" + verb + "' URI: '" + uri + "' Payload: " + requestPayload);
-      Map<String, String> mAuthHeaders = httpClientRequestSigner.generateRequestHeaders(verb, uri, requestPayload);
-      mAuthHeaders.entrySet().forEach((header) -> request.headers().add(header.getKey(), header.getValue()));
-    }
-  }
 
   private HttpResponse appStatus() {
     byte[] bodyInBytes = buildInfo.getBytes(Charset.forName("UTF-8"));
--- mauth-proxy/src/main/java/com/mdsol/mauth/proxy/ProxyServer.java
+++ mauth-proxy/src/main/java/com/mdsol/mauth/proxy/ProxyServer.java
@@ -3,73 +3,57 @@ package com.mdsol.mauth.proxy;
 import com.mdsol.mauth.apache.HttpClientRequestSigner;
 import com.typesafe.config.ConfigFactory;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.http.FullHttpRequest;
-import io.netty.handler.codec.http.HttpObject;
 import io.netty.handler.codec.http.HttpRequest;
-import io.netty.handler.codec.http.HttpResponse;
 import org.littleshoot.proxy.HttpFilters;
-import org.littleshoot.proxy.HttpFiltersAdapter;
 import org.littleshoot.proxy.HttpFiltersSourceAdapter;
 import org.littleshoot.proxy.HttpProxyServer;
+import org.littleshoot.proxy.HttpProxyServerBootstrap;
 import org.littleshoot.proxy.extras.SelfSignedMitmManager;
 import org.littleshoot.proxy.impl.DefaultHttpProxyServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.charset.Charset;
-import java.util.Map;
 
 public class ProxyServer {
-  private static final Logger logger = LoggerFactory.getLogger(ProxyServer.class);
   private final ProxyConfig proxyConfig;
 
   private final HttpClientRequestSigner httpClientRequestSigner;
   private HttpProxyServer httpProxyServer;
 
-  public ProxyServer(ProxyConfig proxyConfig) throws IOException, URISyntaxException {
+  ProxyServer(ProxyConfig proxyConfig) throws IOException, URISyntaxException {
     this.proxyConfig = proxyConfig;
 
     httpClientRequestSigner = new HttpClientRequestSigner(
         proxyConfig.getAppUuid(),
         proxyConfig.getPrivateKey()
     );
-
   }
 
-  public void serve() {
-    httpProxyServer = DefaultHttpProxyServer.bootstrap()
-        .withPort(proxyConfig.getProxyPort())
-        .withManInTheMiddle(new SelfSignedMitmManager())
-        .withFiltersSource(new HttpFiltersSourceAdapter() {
-          @Override
-          public int getMaximumRequestBufferSizeInBytes() {
-            return proxyConfig.getBufferSizeInByes();
-          }
+  void serve() {
+    final HttpProxyServerBootstrap bootstrap = DefaultHttpProxyServer.bootstrap().withPort(proxyConfig.getProxyPort());
 
-          public HttpFilters filterRequest(HttpRequest originalRequest, ChannelHandlerContext ctx) {
-            return new MAuthRequestFilter(originalRequest, httpClientRequestSigner);
-          }
+    httpProxyServer = bootstrap.withFiltersSource(new HttpFiltersSourceAdapter() {
+      @Override
+      public int getMaximumRequestBufferSizeInBytes() {
+        return proxyConfig.getBufferSizeInByes();
+      }
 
-        })
+      public HttpFilters filterRequest(HttpRequest originalRequest, ChannelHandlerContext clientCtx) {
+        return new MAuthForwardRequestFilter(originalRequest, httpClientRequestSigner);
+      }
+    })
         .start();
   }
 
-  public void stop() {
+  void stop() {
     if (httpProxyServer != null) {
       httpProxyServer.stop();
     }
   }
 
-  public void abort() {
-    if (httpProxyServer != null) {
-      httpProxyServer.abort();
-    }
-  }
-
-  public int getPort() {
+  int getPort() {
     return httpProxyServer.getListenAddress().getPort();
   }
 
