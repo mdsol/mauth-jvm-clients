75a8854c888223e48316bdbaea0765b3092b02db
mcallonalvarez <mcallonalvarez@mdsol.com>
2018-07-20 13:46:17 +0100

    [MCC-403926] Correct codacy format errors

--- modules/mauth-signer-akka-http/src/test/scala/com/mdsol/mauth/http/ImplicitsTest.scala
+++ modules/mauth-signer-akka-http/src/test/scala/com/mdsol/mauth/http/ImplicitsTest.scala
@@ -11,22 +11,22 @@ class ImplicitsTest extends WordSpec with Matchers {
   "Implicits fromSignedRequestToHttpRequest" should {
 
     "Generate a POST HttpRequest from a SignedRequest" in {
-      val signedRequest = SignedRequest(UnsignedRequest(httpMethod = "POST", uri = new URI("/")), "X_MWS_AUTHENTICATION_HEADER_VALUE", "X_MWS_TIME_HEADER_VALUE")
+      val signedRequest = SignedRequest(UnsignedRequest(httpMethod = "POST", uri = new URI("/")), "", "")
       fromSignedRequestToHttpRequest(signedRequest).method.toString() should be("HttpMethod(POST)")
     }
 
     "Generate a GET HttpRequest from a SignedRequest when non-uppercase method specified" in {
-      val signedRequest = SignedRequest(UnsignedRequest(httpMethod = "post", uri = new URI("/")), "X_MWS_AUTHENTICATION_HEADER_VALUE", "X_MWS_TIME_HEADER_VALUE")
+      val signedRequest = SignedRequest(UnsignedRequest(httpMethod = "post", uri = new URI("/")), "", "")
       fromSignedRequestToHttpRequest(signedRequest).method.toString() should be("HttpMethod(GET)")
     }
 
     "Generate a GET HttpRequest from a SignedRequest when http method does not exist" in {
-      val signedRequest = SignedRequest(UnsignedRequest(httpMethod = "INVENTED", uri = new URI("/")), "X_MWS_AUTHENTICATION_HEADER_VALUE", "X_MWS_TIME_HEADER_VALUE")
+      val signedRequest = SignedRequest(UnsignedRequest(httpMethod = "INVENTED", uri = new URI("/")), "", "")
       fromSignedRequestToHttpRequest(signedRequest).method.toString() should be("HttpMethod(GET)")
     }
 
     "Generate a GET HttpRequest from a SignedRequest if not http method specified" in {
-      val signedRequest = SignedRequest(UnsignedRequest(uri = new URI("/")), "X_MWS_AUTHENTICATION_HEADER_VALUE", "X_MWS_TIME_HEADER_VALUE")
+      val signedRequest = SignedRequest(UnsignedRequest(uri = new URI("/")), "", "")
       fromSignedRequestToHttpRequest(signedRequest).method.toString() should be("HttpMethod(GET)")
     }
   }
